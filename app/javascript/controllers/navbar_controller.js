753e745e96d7faa0894157a961129e127aa2fe16
felix1251 <domhack123@gmail.com>
2022-11-12 15:53:09 +0800

    event fucntions

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,11 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
-  static targets = ["openUserMenu", "openNavItems"];
+  static targets = ["openUserMenu", "openNavItems", "userAuthLink"];
 
   connect() {
     this.openUserMenuTarget.addEventListener('click', this.toggeDropdownMenu)
     this.openNavItemsTarget.addEventListener('click', this.toggeNavItems)
+
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('modal-trigger').click()
+        leave(document.getElementById('menu-dropdown-items'))
+      })
+    })
   }
 
   toggeDropdownMenu(){
