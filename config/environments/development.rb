a68f05c97e9c0bb4655a4b4a897ad8f05d9744d3
“felix1251” <domhack123@gmail.com>
2023-03-15 10:27:15 +0800

    tailwind watch not working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.assets.debug = true
   # Show full error reports.
   config.consider_all_requests_local = true
 
