b87689cb11dfd4a13f3afce08ed63cc4297e6a71
“felix1251” <domhack123@gmail.com>
2023-03-15 17:50:53 +0800

    navbar animation

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,26 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 // import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
-  // static targets = ["openUserMenu", "openNavItems", "userAuthLink"];
-
-  // connect() {
-  //   this.openUserMenuTarget.addEventListener('click', this.toggeDropdownMenu)
-  //   this.openNavItemsTarget.addEventListener('click', this.toggeNavItems)
-
-  //   this.userAuthLinkTargets.forEach((link) => {
-  //     link.addEventListener('click', (e) => {
-  //       e.preventDefault();
-  //       document.getElementById('modal-trigger').click()
-  //       leave(document.getElementById('menu-dropdown-items'))
-  //     })
-  //   })
-  // }
-
-  // toggeDropdownMenu(){
-  //   toggle(document.getElementById('menu-dropdown-items'))
-  // }
-
-  // toggeNavItems(){
-  //   toggle(document.getElementById('menu-nav-items'))
-  // }
+  connect() {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY==0){
+        this.element.classList.remove("bg-gray-700");
+      } else {
+        this.element.classList.add("bg-gray-700");
+      }
+    });
+  }
 }
