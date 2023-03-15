eb75e23988c2c3a86662d88af56f361b203e4a55
“felix1251” <domhack123@gmail.com>
2023-03-15 13:23:56 +0800

    dark mode

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,7 @@
 // const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  darkMode: 'class',
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
@@ -8,6 +9,13 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    extend: {
+      colors: {
+        main: "#01345B",
+        primary: "#5CE0D8",
+        secondary: "#FFCF43",
+      },
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
