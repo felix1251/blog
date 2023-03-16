fccde94a8f33801e7e037d5b3b4abb8a22eba459
“felix1251” <domhack123@gmail.com>
2023-03-16 10:09:20 +0800

    navbar intersection detect

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,12 +3,37 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.navContent = document.getElementById("nav-content")
+    this.init = false
+    this.navBackground = document.getElementById("nav-background")
+    this.secondSection = document.getElementById("second-section")
+    this.thirdSection = document.getElementById("third-section")
+
     window.addEventListener("scroll", () => {
+      const value = ((this.element.clientHeight/100)*window.scrollY)/100
+      this.navBackground.style.opacity = value.toFixed(1) 
+      const a = this.element.getBoundingClientRect();
+      const b = this.secondSection.getBoundingClientRect();
+      const c = this.thirdSection.getBoundingClientRect();
+      const intersectOnSecondSection = a.top <= b.top + b.height && a.top + a.height > b.top
+      const intersectOnThirdSection = a.top <= c.top + c.height && a.top + a.height > c.top
+
+      if (intersectOnSecondSection && !intersectOnThirdSection) {
+        this.navBackground.classList.remove("bg-secondary");
+        this.navBackground.classList.add("bg-gray-800");
+        this.element.classList.replace("text-gray-800", "text-secondary",)
+        return
+      }
+
       if(window.scrollY==0){
-        this.element.classList.remove("bg-secondary");
-      } else {
-        this.element.classList.add("bg-secondary");
+        this.init = false
+        this.navBackground.classList.remove("bg-secondary");
+        this.element.classList.replace("text-gray-800", "text-secondary")
+      }
+
+      if(window.scrollY > 0) {
+        this.init = true
+        this.navBackground.classList.add("bg-secondary");
+        this.element.classList.replace("text-secondary", "text-gray-800")
       }
     });
   }
