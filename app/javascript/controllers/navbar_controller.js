0aa4edcc64a426539c7c9e71ca2d2792c43dd2e0
“felix1251” <domhack123@gmail.com>
2023-03-16 15:04:39 +0800

    .

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   connect() {
     this.navBackground = document.getElementById("nav-background")
     this.secondSection = document.getElementById("second-section")
-    this.thirdSection = document.getElementById("third-section")
+    this.forthSection = document.getElementById("forth-section")
 
     window.addEventListener("scroll", () => {
       const navheight = this.element.clientHeight
@@ -19,11 +19,12 @@ export default class extends Controller {
 
       const navComponent = this.element.getBoundingClientRect();
       const secondSection = this.secondSection.getBoundingClientRect();
-      const thirdSection = this.thirdSection.getBoundingClientRect();
-      const intersectOnSecondSection = navComponent.top <= secondSection.top + secondSection.height && navComponent.top + navComponent.height > secondSection.top
-      const intersectOnThirdSection = navComponent.top <= thirdSection.top + thirdSection.height && navComponent.top + navComponent.height > thirdSection.top
+      const forthSection = this.forthSection.getBoundingClientRect();
 
-      if(winSrollY == 0 || (intersectOnSecondSection && !intersectOnThirdSection)){
+      const intersectOnSecondSection = navComponent.bottom <= secondSection.top + secondSection.height && navComponent.top + navComponent.height > secondSection.top
+      const intersectOnForthSection = navComponent.bottom <= forthSection.top + forthSection.height && navComponent.top + navComponent.height > forthSection.top
+
+      if(winSrollY == 0 || intersectOnSecondSection || intersectOnForthSection){
         this.navBackground.classList.remove("bg-secondary");
         this.navBackground.classList.add("bg-gray-800");
         this.element.classList.replace("text-gray-800", "text-secondary")
