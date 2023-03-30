f5f1fcba552d7fa27e4585a50123a92054fa4af2
“felix1251” <domhack123@gmail.com>
2023-03-30 13:21:56 +0800

    remove unused js controllers

--- app/javascript/controllers/blogs_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-// import {enter, leave, toggle} from 'el-transition'
-
-export default class extends Controller {
-  connect() { 
-    console.log("im in blogs")
-  }
-};
