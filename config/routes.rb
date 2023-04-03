4f4dbb31890850074f1d9436625015e47ab474b5
“felix1251” <domhack123@gmail.com>
2023-04-03 13:24:47 +0800

    check box custom css

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
 
   resources :projects
   resources :blogs
-
-  devise_scope :user do
-    # Redirests signing out users back to sign-in
-    get "users", to: "devise/sessions#new"
-  end
   
-  devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_in => "admin/login", :sign_out => "logout" }
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
