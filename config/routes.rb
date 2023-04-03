37572f31b9995502c5b47dbc7cb40068ad93a486
“felix1251” <domhack123@gmail.com>
2023-04-03 12:56:09 +0800

    sign-in device custom ui

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   resources :projects
   resources :blogs
+
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
   devise_for :users
 
   match "/404", to: "errors#not_found", via: :all
