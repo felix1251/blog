7aa149a03a481d3158a9719f2ce85f668988052f
“felix1251” <domhack123@gmail.com>
2023-04-04 15:33:31 +0800

    display image

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
     validates :title, uniqueness: true, presence: true
     validates :content, presence: true
     validates :project_start, presence: true
+    has_one_attached :img
 end
