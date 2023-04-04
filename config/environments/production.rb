85a57d6c42c3c3f38d1971c0319c5507fa201721
“felix1251” <domhack123@gmail.com>
2023-04-04 16:21:33 +0800

    custom active storage path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.action_view.image_loading = "lazy"
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
