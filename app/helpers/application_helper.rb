c335c18df26a2e07c91d6342e86880f2545da837
“felix1251” <domhack123@gmail.com>
2023-04-11 11:23:42 +0800

    use helper to display sections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
     include Pagy::Frontend
+
+    def main_layout
+        render "components/templates/main_layout" 
+    end
+    
+    def main_section
+        render "components/molecules/home_section_one"
+    end
+
+    def project_section
+        render "components/molecules/home_section_two"
+    end
+
+    def service_section
+        render "components/molecules/home_section_three"
+    end
 end
