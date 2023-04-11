9fe6ed12d122221645ca3be56be1fdef2f20353f
“felix1251” <domhack123@gmail.com>
2023-04-11 11:31:54 +0800

    move to appropriate helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,17 @@
 module HomeHelper
+    def main_layout
+        render "components/templates/main_layout" 
+    end
+    
+    def main_section
+        render "components/molecules/home_section_one"
+    end
+
+    def project_section
+        render "components/molecules/home_section_two"
+    end
+
+    def service_section
+        render "components/molecules/home_section_three"
+    end
 end
