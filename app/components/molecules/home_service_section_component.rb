e041f2b33dbd7212a6457452e36230881d838af1
“felix1251” <domhack123@gmail.com>
2023-04-13 16:25:32 +0800

    services card

--- app/components/molecules/home_service_section_component.rb
+++ app/components/molecules/home_service_section_component.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Molecules::HomeServiceSectionComponent < ViewComponent::Base
-    delegate :section_header_component, to: :helpers
+    delegate :section_header_component, :service_card_component, to: :helpers
 end
---
+++ test/components/atoms/service_card_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Atoms::ServiceCardComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Atoms::ServiceCardComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
