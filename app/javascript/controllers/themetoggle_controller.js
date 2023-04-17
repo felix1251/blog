d4c87d328e86cf59ff57ff9b6c979b05e2f55e16
“felix1251” <domhack123@gmail.com>
2023-04-17 15:37:19 +0800

    adjust quate

--- app/javascript/controllers/themetoggle_controller.js
+++ app/javascript/controllers/themetoggle_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus";
-// import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
     static targets = ["lightIcon", "darkIcon"];
