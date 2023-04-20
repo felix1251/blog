745a7f7101d190ea6ffe64a5fba6b03940335d06
“felix1251” <domhack123@gmail.com>
2023-04-20 15:16:47 +0800

    project card date display logic

--- app/components/atoms/project_card_component.rb
+++ app/components/atoms/project_card_component.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class Atoms::ProjectCardComponent < ViewComponent::Base
+
   def initialize(project:)
     @project = project
+  end
+
+  def date_display
+    formatted_start = @project.project_start.strftime("%b %Y")
+    formatted_end = @project.project_end&.strftime("%b %Y") || "present"
+
+    unless formatted_start == formatted_end
+      "#{formatted_start + " - " + formatted_end || "present"}"
+    else
+      formatted_start
+    end
   end
 end
