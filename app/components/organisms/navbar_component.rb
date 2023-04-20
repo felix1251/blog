afe5a92ea5ca7cc5b97993c4b34242148b3a0fb0
“felix1251” <domhack123@gmail.com>
2023-04-20 10:02:49 +0800

    highlight in sub actions in coterller navs

--- app/components/organisms/navbar_component.rb
+++ app/components/organisms/navbar_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Organisms::NavbarComponent < ViewComponent::Base
-    delegate :user_signed_in?, :current_page?, :theme_switcher_component, :navlink_component,
-        :destroy_user_session_path, :devise_controller?, :navbutton_component, to: :helpers
+    delegate :user_signed_in?, :theme_switcher_component, :navlink_component,
+    :destroy_user_session_path, :devise_controller?, :navbutton_component, to: :helpers
 end
