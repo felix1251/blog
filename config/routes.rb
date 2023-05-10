9c5e72df38d1c4fdb4d1939bf84f4655793da53f
felix1251 <domhack123@gmail.com>
2023-05-10 22:15:41 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :blogs
   resources :projects
+  resources :about
+
+  # The priority is based upon order of creation:
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
